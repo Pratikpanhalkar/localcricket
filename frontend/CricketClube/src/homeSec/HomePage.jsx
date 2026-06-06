b6b31f5a7b67d74ca967c50bcd3ca1781ad3b2e2
pratik panhalkar <pratikpanhalkars@gmail.com>
2026-06-07 00:19:15 +0530

    fix register import everywhere

--- frontend/CricketClube/src/homeSec/HomePage.jsx
+++ frontend/CricketClube/src/homeSec/HomePage.jsx
@@ -3,7 +3,7 @@ import Navbar from "./Navbar";
 import Hero from "./Hero";
 import FeaturesSection from "./FeaturesSection";
 import Footer from "./Footer";
-import Register from "./register";
+import Register from "./Register";
 function HomePage(){
     return(
         <>
